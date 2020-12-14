422977a8b08c5173a5ff76a4adac1a5d3a0f594e
mieubrisse <mieubrisse@gmail.com>
2020-12-13 19:37:34 -0600

    Fixing things that changes with Gradle 6.7

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -7,9 +7,9 @@ cli_filepath="${build_dirpath}/cli"
 test_resources_dirpath="${root_dirpath}/src/test/resources/examples"
 
 extra_args="${1+"${@}"}"
-"${root_dirpath}/gradlew" build run --args="
-    --gov-constants ${test_resources_dirpath}/gov-constants-example.yml 
-    --scenarios ${test_resources_dirpath}/scenarios-example.yml 
-    --assets ${test_resources_dirpath}/assets-example.yml 
-    --projections ${test_resources_dirpath}/projections-example.yml
+"${root_dirpath}/gradlew" build run --args=" \
+    --gov-constants ${test_resources_dirpath}/gov-constants-example.yml \
+    --scenarios ${test_resources_dirpath}/scenarios-example.yml \
+    --assets ${test_resources_dirpath}/assets-example.yml \
+    --projections ${test_resources_dirpath}/projections-example.yml \
     ${extra_args}"
