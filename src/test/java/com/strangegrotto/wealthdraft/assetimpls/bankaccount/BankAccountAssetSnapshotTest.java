e1cffd3ac9bb22941bbcc9bf47610e5f869a3fc2
mieubrisse <mieubrisse@gmail.com>
2021-01-03 17:41:45 -0600

    Fix test findbugs errors

--- src/test/java/com/strangegrotto/wealthdraft/assetimpls/bankaccount/BankAccountAssetSnapshotTest.java
+++ src/test/java/com/strangegrotto/wealthdraft/assetimpls/bankaccount/BankAccountAssetSnapshotTest.java
@@ -12,7 +12,7 @@ public class BankAccountAssetSnapshotTest {
     @Test
     public void testProjectionMath() {
         var expectedResult = BigDecimal.valueOf(100.25);
-        var snapshot = ImmBankAccountAssetSnapshot.of(new BigDecimal(100), new BigDecimal(0.03));
+        var snapshot = ImmBankAccountAssetSnapshot.of(new BigDecimal(100), BigDecimal.valueOf(0.03));
         var newSnapshot = snapshot.projectOneMonth();
         var roundedNewBalance = newSnapshot.getValue().setScale(2, RoundingMode.HALF_EVEN);
         Assert.assertEquals(
@@ -24,9 +24,9 @@ public class BankAccountAssetSnapshotTest {
     @Test
     public void testChangeApplication() {
         BigDecimal newBalance = new BigDecimal(50);
-        BigDecimal newInterestRate = new BigDecimal(0.05);
+        BigDecimal newInterestRate = BigDecimal.valueOf(0.05);
 
-        var snapshot = ImmBankAccountAssetSnapshot.of(new BigDecimal(100), new BigDecimal(0.03));
+        var snapshot = ImmBankAccountAssetSnapshot.of(new BigDecimal(100), BigDecimal.valueOf(0.03));
         var balanceChangeOperation = ImmAssetParameterChange.of(newBalance,AssetParameterChangeValueOperation.SET);
         var interestRateChangeOperation = ImmAssetParameterChange.of(newInterestRate, AssetParameterChangeValueOperation.SET);
         var change = ImmBankAccountAssetChange.builder()
