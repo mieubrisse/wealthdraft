1933019c2b5dd8ec9a104386e5c42648b4344cb4
mieubrisse <mieubrisse@gmail.com>
2021-01-10 16:06:45 -0600

    Add comments explaining why we're passing allFilters around in AssetFilter

--- src/main/java/com/strangegrotto/wealthdraft/assetallocation/datamodel/filters/AssetFilter.java
+++ src/main/java/com/strangegrotto/wealthdraft/assetallocation/datamodel/filters/AssetFilter.java
@@ -14,6 +14,11 @@ import java.util.*;
         @JsonSubTypes.Type(EmbeddedFilterAssetFilter.class)
 })
 public interface AssetFilter {
+    // NOTE: We need to pass the allFilters argument so that the EmbeddedFilterAssetFilter class can work,
+    //  but I'm not super happy with needing to do this. Ideally, rather than needing to pass this as an argument
+    //  to *every* call, it would be instance-specific data given only to the embedded filter class
+    //  at time of instantion.. but this would require writing custom deserializers for the TargetAssetAllocations
+    //  and AssetFilter classes
     Map<String, Asset> apply(Map<String, AssetFilter> allFilters, Map<String, Asset> input);
 
     /**
@@ -23,5 +28,10 @@ public interface AssetFilter {
      * @param parentFilters The names of the parent filters that have been seen so far
      * @return The cycle, if one exists
      */
+    // NOTE: We need to pass the allFilters argument so that the EmbeddedFilterAssetFilter class can work,
+    //  but I'm not super happy with needing to do this. Ideally, rather than needing to pass this as an argument
+    //  to *every* call, it would be instance-specific data given only to the embedded filter class
+    //  at time of instantion.. but this would require writing custom deserializers for the TargetAssetAllocations
+    //  and AssetFilter classes
     Optional<List<String>> checkForCycles(Map<String, AssetFilter> allFilters, LinkedHashSet<String> parentFilters);
 }
