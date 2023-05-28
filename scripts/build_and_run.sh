20131110e7a3d72c8ac2c2b96b370b4572b18325
mieubrisse <mieubrisse@gmail.com>
2023-05-28 19:26:21 -0300

    Make build-and-run Dockerized

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -4,17 +4,23 @@ root_dirpath="$(dirname "${script_dirpath}")"
 build_dirpath="${root_dirpath}/build"
 cli_filepath="${build_dirpath}/cli"
 
-test_resources_dirpath="${root_dirpath}/src/test/resources/examples"
+project_mount_dirpath="/home/gradle/project"
+test_resources_dirpath="${project_mount_dirpath}/src/test/resources/examples"
 
 extra_args="${1+"${@}"}"
-"${root_dirpath}/gradlew" build run --args=" \
-    --gov-constants ${test_resources_dirpath}/gov-constants.yml \
-    --scenarios ${test_resources_dirpath}/scenarios.yml \
-    --assets ${test_resources_dirpath}/assets.yml \
-    --assets-history ${test_resources_dirpath}/assets-history.yml \
-    --projections ${test_resources_dirpath}/projections.yml \
-    --asset-allocations ${test_resources_dirpath}/asset-allocations.yml \
-    --filters ${test_resources_dirpath}/filters.yml \
-    `# Render all scenarios during development, to make sure things work` \
-    --all \
-    ${extra_args}"
+docker run --rm \
+    -u gradle \
+    -v "${root_dirpath}:${project_mount_dirpath}" \
+    -w "${project_mount_dirpath}" \
+    gradle:6-jdk11-focal \
+    gradle build run --args=" \
+        --gov-constants ${test_resources_dirpath}/gov-constants.yml \
+        --scenarios ${test_resources_dirpath}/scenarios.yml \
+        --assets ${test_resources_dirpath}/assets.yml \
+        --assets-history ${test_resources_dirpath}/assets-history.yml \
+        --projections ${test_resources_dirpath}/projections.yml \
+        --asset-allocations ${test_resources_dirpath}/asset-allocations.yml \
+        --filters ${test_resources_dirpath}/filters.yml \
+        `# Render all scenarios during development, to make sure things work` \
+        --all \
+        ${extra_args}"
